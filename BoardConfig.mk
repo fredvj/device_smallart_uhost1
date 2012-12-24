a59c2dd9fd66003f876c7b42bd389fc0d963ce81
fredvj <fredvjupiter@googlemail.com>
2012-12-24 02:46:33 +0100

    Kernel command line & recovery reboot commands

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,11 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/smallart/uboot1/BoardConfigVendor.mk
 
-TARGET_NO_BOOTLOADER := true
+BOARD_USES_UBOOT := true
+
+# TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
 TARGET_BOARD_PLATFORM := exDroid
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
@@ -14,7 +18,7 @@ TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_CMDLINE := console=ttyS0,115200 rw init=/init loglevel=8
 BOARD_KERNEL_BASE := 0x40000000
 BOARD_KERNEL_PAGESIZE := 2048
 
@@ -26,6 +30,10 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/smallart/uboot1/prebuilt/kernel
+
+TARGET_USERIMAGES_USE_EXT4 := true
+
+TARGET_RECOVERY_PRE_COMMAND := "echo -n boot-recovery | busybox dd of=/dev/block/nandf count=1 conv=sync; sync;"
 
 #BOARD_HAS_NO_SELECT_BUTTON := true
 # Use this flag if the board has a ext4 partition larger than 2gb
