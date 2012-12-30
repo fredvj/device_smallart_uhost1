923ad0ed9ca80e5934e34cb7a3a2da8b57df4fd6
fredvj <fredvjupiter@googlemail.com>
2012-12-30 11:18:18 +0100

    Script to create an uImage from our kernel

---
+++ recovery/mk_uimage_kernel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Copyright (C) 2012, fredvj
+#
+
+set -x
+
+# Set the required folders
+
+OUTDIR=../../../../out/target/product/uboot1
+KERNELDIR=../prebuilt
+
+# Run mkimage to create an uImage
+
+mkimage -A arm -O linux -T kernel -C none -a 40008000 -e 40008000 -n "Android Kernel Image" -d "${KERNELDIR}/kernel" "${OUTDIR}/uImage"
