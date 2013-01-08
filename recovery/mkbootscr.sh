5e7ad079d738d5ac7fd3495314b26d01027b8619
fredvj <fredvjupiter@googlemail.com>
2013-01-08 20:44:13 +0100

    Changed script to create boot.scr from boot.cmd to store output in prebuilt folder

--- recovery/mkbootscr.sh
+++ recovery/mkbootscr.sh
@@ -3,4 +3,4 @@
 # Copyright (C) 2012, fredvj
 #
 
-mkimage -A arm -O u-boot -T script -C none -n "boot" -d boot.cmd boot.scr
+mkimage -A arm -O u-boot -T script -C none -n "boot" -d boot.cmd ../prebuilt/boot.scr
