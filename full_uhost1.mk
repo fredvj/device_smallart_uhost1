409bf40fdf82094b30508767b3871928f7b8f5ba
fredvj <fredvjupiter@googlemail.com>
2013-01-18 19:37:11 +0100

    Added permissions & init scripts

--- full_uhost1.mk
+++ full_uhost1.mk
@@ -1,11 +1,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
 
 $(call inherit-product-if-exists, vendor/smallart/uhost1/uhost1-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/smallart/uhost1/overlay
+
+# Inherit dalvik parameters
+$(call inherit-product, frameworks/base/build/phone-hdpi-512-dalvik-heap.mk)
 
 LOCAL_PATH := device/smallart/uhost1
 ifeq ($(TARGET_PREBUILT_KERNEL),)
@@ -18,6 +21,31 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_KERNEL):kernel
 
 $(call inherit-product, build/target/product/full.mk)
+
+# Vold config, boot logo & init scripts
+
+PRODUCT_COPY_FILES += \
+        device/smallart/uhost1/configuration/vold.fstab:system/etc/vold.fstab \
+        device/smallart/uhost1/configuration/init.sun4i.rc:root/init.sun4i.rc \
+        device/smallart/uhost1/configuration/init.sun4i.usb.rc:root/init.sun4i.usb.rc \
+        device/smallart/uhost1/configuration/ueventd.sun4i.rc:root/ueventd.sun4i.rc \
+        device/smallart/uhost1/configuration/media_profiles.xml:system/etc/media_profiles.xml
+
+
+# Permissions
+
+PRODUCT_COPY_FILES += \
+        frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+        frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+        frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+        frameworks/base/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+        frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+        frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+        frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+        frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+        frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+        frameworks/base/data/etc/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+        packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_uhost1
