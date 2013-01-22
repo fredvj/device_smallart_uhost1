ae34bf3f59d1862c9ad00850b7fca53b53a48adf
fredvj <fredvjupiter@googlemail.com>
2013-01-23 00:32:11 +0100

    Make sure the Smallart Uhost1 specific settings are just written once

--- configuration/default-settings.sh
+++ configuration/default-settings.sh
@@ -4,15 +4,29 @@
 #
 # Insert values that cannot be set from an overlay directly into the database
 
+# set -x
+
 SQLCMD=/system/xbin/sqlite3
 DATABASE=/data/data/com.android.providers.settings/databases/settings.db
 
-$SQLCMD $DATABASE "insert or replace into system(name, value) values('stay_on_while_plugged_in', '1')";
-$SQLCMD $DATABASE "insert or replace into system(name, value) values('time_12_24', '24')";
-$SQLCMD $DATABASE "insert or replace into system(name, value) values('status_bar_battery', '2')";
-$SQLCMD $DATABASE "insert or replace into system(name, value) values('expanded_widget_buttons_tablet', 'pref_bluetooth_toggle\|pref_wifi_toggle\|pref_sound_toggle')";
-$SQLCMD $DATABASE "insert or replace into system(name, value) values('notification_sound', 'content://media/internal/audio/media/59')";
-$SQLCMD $DATABASE "insert or replace into system(name, value) values('mock_location', '0')";
+# Make sure we just set the default once during initial setup
+INITIALIZED=`$SQLCMD $DATABASE "select value from system where name='smart_tv_initialized';"`
 
-$SQLCMD $DATABASE "insert or replace into secure(name, value) values('lockscreen.disabled', '1')";
+if [ "$INITIALIZED" = "" ]
+then
+	echo "Initializing defaults for Smallart Uhost1"
+
+	$SQLCMD $DATABASE "insert or replace into system(name, value) values('stay_on_while_plugged_in', '1');"
+	$SQLCMD $DATABASE "insert or replace into system(name, value) values('time_12_24', '24');"
+	$SQLCMD $DATABASE "insert or replace into system(name, value) values('status_bar_battery', '2');"
+	$SQLCMD $DATABASE "insert or replace into system(name, value) values('expanded_widget_buttons_tablet', 'pref_bluetooth_toggle\|pref_wifi_toggle\|pref_sound_toggle');"
+	$SQLCMD $DATABASE "insert or replace into system(name, value) values('notification_sound', 'content://media/internal/audio/media/59');"
+	$SQLCMD $DATABASE "insert or replace into system(name, value) values('mock_location', '0');"
+
+	$SQLCMD $DATABASE "insert or replace into secure(name, value) values('lockscreen.disabled', '1');"
+
+	$SQLCMD $DATABASE "insert or replace into system(name, value) values('smart_tv_initialized', '1');"
+else
+	echo "Defaults for Smallart Uhost1 are already initialized"
+fi
 
