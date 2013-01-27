04f2c4b9423afa3c5526b9c026e26c2a5509d960
fredvj <fredvjupiter@googlemail.com>
2013-01-27 20:16:36 +0100

    Played with JB bootanimation settings - minor name changes

--- cm.mk
+++ cm.mk
@@ -3,6 +3,8 @@ PRODUCT_RELEASE_NAME := uhost1
 
 # Select the right boot animation
 TARGET_BOOTANIMATION_NAME := horizontal-1024x600
+# TARGET_SCREEN_WIDTH := 1280
+# TARGET_SCREEN_HEIGHT := 720
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
@@ -13,6 +15,6 @@ $(call inherit-product, device/smallart/uhost1/full_uhost1.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := uhost1
 PRODUCT_NAME := cm_uhost1
-PRODUCT_BRAND := smallart
-PRODUCT_MODEL := uhost1
-PRODUCT_MANUFACTURER := smallart
+PRODUCT_BRAND := Smallart
+PRODUCT_MODEL := UHOST1
+PRODUCT_MANUFACTURER := Smallart
