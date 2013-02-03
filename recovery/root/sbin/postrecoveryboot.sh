8c495c9aafb7a65dbcc820551e27b040a934ccb5
fredvj <fredvjupiter@googlemail.com>
2013-02-03 21:54:01 +0100

    Break out of the recovery reboot loop

--- recovery/root/sbin/postrecoveryboot.sh
+++ recovery/root/sbin/postrecoveryboot.sh
@@ -1,6 +1,8 @@
 #!/sbin/sh
 #
-# Copyright (C) 2012, fredvj
+# Copyright (C) 2012, 2013 fredvj
 #
-# Empty placeholder
+# Clear misc partition - Allow reboot to ROM
 #
+/sbin/busybox dd bs=512 count=1 if=/dev/zero of=/dev/block/nandf
+
